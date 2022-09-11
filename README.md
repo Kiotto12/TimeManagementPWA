9c7d5ecb2e17ee2c8b845d1d9e50b16fc3678800
Kiotto <kottoco12@gmail.com>
2022-09-10 21:10:04 -0600

    0.0.00.0.01

---
+++ README.md
@@ -0,0 +1,9 @@
+# Learn PWA - Web App Manifest
+
+This example is part of the [[Learn PWA - Web App Manifest Chapter]](https://web.dev/learn/pwa/web-app-manifest) guide on [web.dev](https://web.dev).
+
+## License
+
+Copyright 2018 Google LLC.
+
+SPDX-License-Identifier: Apache-2.0
---
+++ dummy-sw.js
@@ -0,0 +1,10 @@
+self.addEventListener("fetch", event => {
+   event.respondWith(
+     caches.match(event.request)
+     .then(cachedResponse => {
+	   // It can update the cache to serve updated content on the next request
+         return cachedResponse || fetch(event.request);
+     }
+   )
+  )
+});
